0661367fec126e6493dabc4caf0e659e10426c01
gabrielchick <92435532+gabrielchick@users.noreply.github.com>
2024-10-29 23:37:01 +0800

    Add files via upload

---
+++ Day05workshopans/src/day05/GameOfLife.java
@@ -0,0 +1,35 @@
+package day05;
+
+public class GameofLife{String gol){
+    goFile = gol;
+
+}}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// java doesnt truly have 2x2 grid array
+// rather java does array of array: char[][] means array of array of characters (read from right to left: (char[])[]) e.g. int [] means array of integers
+
+//board [y] [x + offset]... line is translation of matrix
+
+
+
+
+ 
+//FOR THE GRID
+//check location, give each of the 8 neighbouring positions an algebraic expression that can be looped through a list, for varying central positions
+//check bounds
+//check star
+
+//try to minimise if statements to make program more efficient (especially when already within a loop)
+//but should have enough ifs to make sure the code is clear to the reader
